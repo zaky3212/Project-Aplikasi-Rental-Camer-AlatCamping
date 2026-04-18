90b66f871f68797531d00018c084ea702f243701
zaky3212 <fajarzaky690@gmail.com>
2026-04-18 22:14:37 +0700

    update database penyewaan

--- database/migrations/2026_04_18_142918_create_penyewaans_table.php
+++ database/migrations/2026_04_18_142918_create_penyewaans_table.php
@@ -13,6 +13,27 @@ return new class extends Migration
     {
         Schema::create('penyewaans', function (Blueprint $table) {
             $table->id();
+            
+            // Nama Penyewa
+            $table->string('nama_penyewa');
+            
+            // Relasi ke tabel barang dan kategori
+            // Pastikan tabel barangs dan kategoris sudah ada sebelumnya
+            $table->foreignId('barang_id')->constrained('barangs')->onDelete('cascade');
+            $table->foreignId('kategori_id')->constrained('kategoris')->onDelete('cascade');
+            
+            // Data Kontak & Waktu
+            $table->string('no_hp');
+            $table->date('tanggal_sewa');
+            $table->integer('lama_sewa'); // Dalam satuan hari (sesuai input angka)
+            
+            // Keuangan
+            $table->decimal('total_harga', 12, 2);
+            
+            // Status (Sesuai mockup ada tombol 'Kembali' berwarna hijau)
+            // default 'disewa', bisa berubah jadi 'kembali'
+            $table->enum('status', ['disewa', 'kembali'])->default('disewa');
+            
             $table->timestamps();
         });
     }
